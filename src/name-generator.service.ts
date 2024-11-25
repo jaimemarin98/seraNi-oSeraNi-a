3659e76809d236add20d600dd0f01833906df1bc
jaime marin <33171513+titan9804@users.noreply.github.com>
2024-11-25 16:13:20 -0500

    initial commit

---
+++ src/name-generator.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NameGeneratorService {
+
+  private names: string[] = ['Sara', 'Adara', 'Violeta'];
+
+  constructor() { }
+
+  generateRandomName(): string{
+    const randomIndex = Math.floor(Math.)
+  }
+}
